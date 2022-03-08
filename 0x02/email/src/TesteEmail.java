d6c24403d06fdad0c2a9161903d416c8f9096629
mayaramaaraujo <mayaramaaraujo98@gmail.com>
2022-03-08 09:27:04 -0300

    teste email

--- 0x02/email/src/TesteEmail.java
+++ 0x02/email/src/TesteEmail.java
@@ -1,23 +1,24 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TesteEmail {
 
     @Test
     void testar_email_com_arroba() {
         boolean emailValid = Pessoa.emailValid("email_teste@dominio.com.br");
-        Assertions.assertTrue(emailValid);
+        assertTrue(emailValid);
     }
 
     @Test
     void testar_email_sem_arroba() {
         boolean emailValid = Pessoa.emailValid("testar_email_sem_arroba");
-        Assertions.assertFalse(emailValid);
+        assertFalse(emailValid);
     }
 
     @Test
     void testar_email_mais_50_caracteres() {
         boolean emailValid = Pessoa.emailValid("emailtestemuitolongonaodeveser_valido@dominio.com.br");
-        Assertions.assertEquals(emailValid, false);
+        assertEquals(emailValid, false);
     }
 }
